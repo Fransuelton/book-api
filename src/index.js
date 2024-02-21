21a5829043cff2c52dc205501349cf9f2f6ab5e2
Fransuelton <elton6103@gmail.com>
2024-02-21 09:33:32 -0300

    feat: Add user creation endpoint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const { v4: uuid } = require("uuid");
 
 const app = express();
-
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  console.log(request.method);
-  
-  response.status(200).json({
-    message: "Hello World",
+const users = [];
+
+app.post("/users", (request, response) => {
+  const { name, email } = request.body;
+
+  users.push({
+    name,
+    email,
+    id: uuid(),
   });
+
+  return response.status(201).json({ message: "OK" });
 });
 
 app.listen(3333, () => console.log("Server is running"));
