ac04fc28ef2840db24a7e5de0e2337aa27116c09
Fransuelton <elton6103@gmail.com>
2025-06-26 16:27:21 -0300

    ✨ feat: implement user controller with create, retrieve, update, and delete functionalities

---
+++ src/controllers/userController.js
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
+
+const users = [];
+
+export const createUser = (request, response) => {
+  const { name, email } = request.body;
+
+  const emailAlreadyExists = users.some((user) => user.email === email);
+
+  if (emailAlreadyExists) {
+    return response.status(400).json({ error: "User already exists." });
+  }
+
+  users.push({
+    name,
+    email,
+    id: uuidv4(),
+  });
+
+  return response.status(201).json({ message: "OK" });
+};
+
+export const getUsers = (request, response) => {
+  return response.status(200).json(users);
+};
+
+export const updateUser = (request, response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  const findUser = users.find((user) => user.id === id);
+
+  findUser.name = name;
+  return response.json(users);
+};
+
+export const deleteUser = (request, response) => {
+  const { id } = request.params;
+
+  const user = users.findIndex((user) => user.id === id);
+
+  if (!user) {
+    return response.status(400).json("User not found");
+  }
+
+  users.splice(user, 1);
+
+  return response.json(users);
+};
