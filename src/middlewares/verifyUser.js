b22a2b02d4f08f8af8a073a8f4ae02a6a2f0acfa
Fransuelton <elton6103@gmail.com>
2025-06-26 16:27:27 -0300

    ✨ feat: add verifyUser middleware to check if user exists by email

---
+++ src/middlewares/verifyUser.js
@@ -0,0 +1,13 @@
+export function verifyUser(request, response, next) {
+  const { email } = request.headers;
+
+  const emailAlreadyExists = users.find((user) => user.email === email);
+
+  if (!emailAlreadyExists) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  request.user = emailAlreadyExists;
+
+  return next();
+}
