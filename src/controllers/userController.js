ca8b9bf5777bbd726d0240073985082ee90e0841
Fransuelton <elton6103@gmail.com>
2025-06-26 20:39:22 -0300

    ✨ feat: standardize response format in userController functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,10 @@ export const createUser = (request, response) => {
   const emailAlreadyExists = users.some((user) => user.email === email);
 
   if (emailAlreadyExists) {
-    return response.status(400).json({ error: "User already exists." });
+    return response.status(400).json({
+      success: false,
+      error: "Email already exists.",
+    });
   }
 
   users.push({
@@ -16,11 +19,17 @@ export const createUser = (request, response) => {
     id: uuidv4(),
   });
 
-  return response.status(201).json({ message: "OK" });
+  return response.status(201).json({
+    success: true,
+    message: "User created successfuly.",
+  });
 };
 
 export const getUsers = (request, response) => {
-  return response.status(200).json(users);
+  return response.status(200).json({
+    success: true,
+    data: users,
+  });
 };
 
 export const updateUser = (request, response) => {
@@ -30,7 +39,11 @@ export const updateUser = (request, response) => {
   const findUser = users.find((user) => user.id === id);
 
   findUser.name = name;
-  return response.json(users);
+  return response.status(200).json({
+    success: true,
+    message: "User updated successfully.",
+    data: findUser,
+  });
 };
 
 export const deleteUser = (request, response) => {
@@ -39,10 +52,16 @@ export const deleteUser = (request, response) => {
   const user = users.findIndex((user) => user.id === id);
 
   if (!user) {
-    return response.status(400).json("User not found");
+    return response.status(404).json({
+      success: false,
+      error: "User not found.",
+    });
   }
 
   users.splice(user, 1);
 
-  return response.json(users);
+  return response.status(200).json({
+    success: true,
+    message: "User deleted successfully.",
+  });
 };
